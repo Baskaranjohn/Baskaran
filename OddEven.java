1d3711f447cc6b039be3590e781e8645bb9001cc
Baskaranjohn <baskaranjoseph154@gmail.com>
2017-06-16 12:20:13 +0530

    Add files via upload

---
+++ OddEven.java
@@ -0,0 +1,23 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class OddEven {
+	public static void main(String[] args){
+		int number;
+		System.out.println("Enter the number:");
+	    Scanner s=new Scanner(System.in);
+	    number=s.nextInt();
+	    if(number%2==0){
+	    	System.out.println("The number is even number");
+	    }
+	    else{
+	    	System.out.println("The number is odd number");
+	    }
+	 
+	    }
+		
+		
+	}
+
+  
