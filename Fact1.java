a434c567df6525ddf87850a6f3997c2e961b9e9f
Baskaranjohn <baskaranjoseph154@gmail.com>
2017-07-05 15:33:46 +0530

    Add files via upload

---
+++ Fact1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class Fact1 {
+	public static void main(String[] args){
+		System.out.println("Enter the Number to find the Factorial:");
+		Scanner s=new Scanner(System.in);
+		int num=s.nextInt();
+		int fact=1;
+		for(int i=1;i<=num;i++){
+			fact=fact*i;
+		}
+		System.out.println("The factorial of a given number is:" +fact);
+	}
+
+}
+
+
+
